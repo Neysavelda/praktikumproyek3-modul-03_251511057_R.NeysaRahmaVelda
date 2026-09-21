f8cf10945e386ff202663321f2a2f997ea993aa2
Neysavelda <neztvelda@gmail.com>
2026-09-21 11:30:06 +0700

    feat: add activity model, migration, and seeder (B2)

---
+++ database/seeders/ActivitySeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Activity;
+
+class ActivitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+           Activity::query()->insert([
+       [
+           'title' => 'Workshop Git Dasar',
+           'description' => 'Latihan kolaborasi repository.',
+           'activity_date' => '2026-10-05',
+           'category' => 'Workshop',
+           'status' => 'Planned',
+           'created_at' => now(),
+           'updated_at' => now(),
+       ],
+       [
+           'title' => 'Seminar Web Quality',
+           'description' => 'Pengenalan maintainability dan testing.',
+           'activity_date' => '2026-10-12',
+           'category' => 'Seminar',
+           'status' => 'Planned',
+           'created_at' => now(),
+           'updated_at' => now(),
+       ],
+       [
+           'title' => 'Pelatihan Laravel Basic',
+           'description' => 'Sesi praktik membangun CRUD sederhana.',
+           'activity_date' => '2026-09-28',
+           'category' => 'Pelatihan',
+           'status' => 'Ongoing',
+           'created_at' => now(),
+           'updated_at' => now(),
+       ],
+       [
+           'title' => 'Diskusi Business Logic',
+           'description' => 'Membahas pemisahan tanggung jawab controller dan service.',
+           'activity_date' => '2026-09-20',
+           'category' => 'Diskusi',
+           'status' => 'Ongoing',
+           'created_at' => now(),
+           'updated_at' => now(),
+       ],
+       [
+           'title' => 'Onboarding Mahasiswa Baru',
+           'description' => 'Pengenalan alur kerja praktikum Proyek 3.',
+           'activity_date' => '2026-09-01',
+           'category' => 'Onboarding',
+           'status' => 'Done',
+           'created_at' => now(),
+           'updated_at' => now(),
+       ],
+   ]);
+    }
+}
